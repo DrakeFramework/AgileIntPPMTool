84edbefe83dba5aa5429a79ba014bb8ee76218fe
Drake Nelson <dnelson@KMMFG.local>
2019-11-01 12:56:34 -0500

    set up maven to deploy for tomcat

--- PPMToolFullStack/src/main/java/com/drakenelson/ppmtool/PpmtoolApplication.java
+++ PPMToolFullStack/src/main/java/com/drakenelson/ppmtool/PpmtoolApplication.java
@@ -2,9 +2,10 @@ package com.drakenelson.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PpmtoolApplication {
+public class PpmtoolApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PpmtoolApplication.class, args);
