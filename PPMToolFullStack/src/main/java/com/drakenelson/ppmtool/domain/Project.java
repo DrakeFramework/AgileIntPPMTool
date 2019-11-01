7fad036d476555d847fabaf9b185c8dbb4d567d9
Drake Nelson <dnelson@KMMFG.local>
2019-11-01 10:32:40 -0500

    20191101 created project entity and h2 config

---
+++ PPMToolFullStack/src/main/java/com/drakenelson/ppmtool/domain/Project.java
@@ -0,0 +1,39 @@
+package com.drakenelson.ppmtool.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+    private String projectIdentifier;
+    private String discription;
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpDate() {
+        this.updatedAt = new Date();
+    }
+}
