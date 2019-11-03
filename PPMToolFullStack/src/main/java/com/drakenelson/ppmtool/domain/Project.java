fb7f213ea6caf6b57094dd891b3c895b7ebde2c3
ideophobe <dnelson12@missouriwestern.edu>
2019-11-03 11:46:12 -0600

    added validation to the project object using annotation

--- PPMToolFullStack/src/main/java/com/drakenelson/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/com/drakenelson/ppmtool/domain/Project.java
@@ -15,6 +15,9 @@ import lombok.Data;
  * the actual database.
  *
  * How did people live before Lombok
+ *
+ * the annotation validations will return 500 errors to input json that breaks the validation rules
+ * 
  */
 @Entity
 @Data
@@ -37,7 +40,7 @@ public class Project {
     @Size(min=4, max=5, message ="Please use 4-5 characters")
     @Column(updatable=false, unique=true)
     private String projectIdentifier;
-    
+
     @NotBlank(message="Description is required")
     private String description;
 
@@ -49,11 +52,8 @@ public class Project {
 
     @JsonFormat(pattern="yyyy-mm-dd")
     private Date endDate;
-
-
     @JsonFormat(pattern="yyyy-mm-dd")
     private Date createdAt;
-
     @JsonFormat(pattern="yyyy-mm-dd")
     private Date updatedAt;
 
