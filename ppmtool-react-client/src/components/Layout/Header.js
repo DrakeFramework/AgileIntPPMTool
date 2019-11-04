13000ff13d781f6702b1205da4555df91e01a8ce
Drake Nelson <dnelson@KMMFG.local>
2019-11-04 14:34:07 -0600

    added in basic react stuff and got packages working

---
+++ ppmtool-react-client/src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      //this will have the loginn/logout etc
+      <div>
+        <h1>NavBar</h1>
+      </div>
+    );
+  }
+}
