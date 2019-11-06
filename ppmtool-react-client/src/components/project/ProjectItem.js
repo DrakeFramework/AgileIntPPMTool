e14b17877f2ae48b9acb0ec0fa9680e242297fde
Drake Nelson <dnelson@KMMFG.local>
2019-11-06 13:07:23 -0600

    added project list functionality to the dashboard

--- ppmtool-react-client/src/components/project/ProjectItem.js
+++ ppmtool-react-client/src/components/project/ProjectItem.js
@@ -2,17 +2,22 @@ import React, { Component } from "react";
 
 export default class ProjectItem extends Component {
   render() {
+    //this allows us to set a project item to the props
+    //extract the project from the props
+    const { project } = this.props;
+
     /*<!-- Project Item Component -->*/
     return (
       <div className="container">
         <div className="card card-body bg-light mb-3">
           <div className="row">
             <div className="col-2">
-              <span className="mx-auto">REACT</span>
+              {/* note how it's just wired in from props using curley braces to get the react component from properties */}
+              <span className="mx-auto">{project.projectIdentifier}</span>
             </div>
             <div className="col-lg-6 col-md-4 col-8">
-              <h3>Spring / React Project</h3>
-              <p>Project to create a Kanban Board with Spring Boot and React</p>
+              <h3>{project.projectName}</h3>
+              <p>{project.description}</p>
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
